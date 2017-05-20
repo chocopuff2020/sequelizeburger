839c1ce9cf51ce2899c766c481b0d27dcaa0c88a
Michelle Dai <michelledai2020@gmail.com>
2017-05-20 07:35:09 -0700

    done

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+INSERT INTO burgers_db.burgers (burger_name) values ("Unicorn Burger");
+INSERT INTO burgers_db.burgers (burger_name) values ("Unicorn leg burger");
+INSERT INTO burgers_db.burgers (burger_name) values ("Unicorn Flakes");
+INSERT INTO burgers_db.burgers (burger_name,devoured) values ("Unicorn poop", 1);
